b9e735dbda3944206d44e7c2e5c3679100e8a1d1
Shamil Valiev <i.shamil.valiev@gmail.com>
2019-08-02 10:22:07 +0500

    feat: (configuration) added, init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true,
+        "commonjs": true,
+        "node": true
+    },
+    "extends": "airbnb",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {}
+};
